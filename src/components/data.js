ac8e9126c44bf1b43e532c7681bff3cd20f73747
ncilliez <ncilliez@gmail.com>
2023-01-27 11:38:13 +0100

    projet langue

---
+++ src/components/data.js
@@ -0,0 +1,19 @@
+const languages=[
+    {
+        lang: 'fr-FR',
+        titre:'Bonjour et bienvenue sur notre site',
+        contenu:'Découvrez nos fonctionnalités et nos services!'
+    },
+    {
+        lang: 'en-US',
+        titre:'Hello and welcome on our website',
+        contenu:'Discover our features and services!'
+    },
+    {
+        lang: 'es-ES',
+        titre:'Hola y bienvenido a nuestro sitio web',
+        contenu:'¡Descubre nuestras características y servicios!'
+    }
+  ];
+
+  export default languages;
